fac1b872b114a98e34eb77675d936230a588bd62
Yan Pujante <yan@pongasoft.com>
2018-03-07 08:11:42 -0800

    repo created => project skeleton

---
+++ README.md
@@ -0,0 +1,6 @@
+VST2/3 A/B Switch
+=================
+
+Licensing
+---------
+GPL version 3
---
+++ src/cpp/ABSwitchCIDs.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+
+namespace pongasoft
+{
+namespace VST
+{
+// generated with groovy Integer.toHexString(new Random().nextInt())
+static const ::Steinberg::FUID ABSwitchProcessorUID(0xd785ec88, 0x5ff24f33, 0xcf8b0b7a, 0x4659b6bf);
+
+//------------------------------------------------------------------------
+} // namespace VST
+} // namespace pongasoft
