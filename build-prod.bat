fdcbd738a3bd4ea255580a2e6bc7de7846675cd2
Yan Pujante <ypujante@gmail.com>
2018-08-05 08:01:53 -0700

    download jamba or use local

--- build-prod.bat
+++ build-prod.bat
@@ -10,6 +10,7 @@ cd %BUILD_DIR%
 
 cmake -G"Visual Studio 15 2017 Win64" -DCMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING %SRC_DIR%
 cmake --build . --config Release --target VST_AB_Switch_test
+cmake --build . --config Release --target jamba_test
 ctest -C Release
 
 cmake --build . --config Release --target archive
